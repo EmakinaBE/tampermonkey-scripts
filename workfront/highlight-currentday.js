81d832271f1545cbe45bfd6c4650342e24f7be1d
Jeffrey Vandenbossche <jev@emakina.com>
2021-02-02 14:23:56 +0100

    update header userscript

--- workfront/highlight-currentday.js
+++ workfront/highlight-currentday.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name         Workfront highlight Today
 // @namespace    https://www.emakina.com/
-// @version      0.2
+// @version      1.0
 // @description  Highlight current day
 // @author       Jeffrey Vandenbossche
+// @homepage	 https://gitlab.emakina.net/jev/tampermonkey-scripts
+// @icon         https://emakina.my.workfront.com/static/img/favicon.ico
+// @icon64       https://emakina.my.workfront.com/static/img/favicon.ico
 // @match        https://emakina.my.workfront.com/timesheet/*
 // @match        https://emakina.my.workfront.com/timesheets/current*
 // @grant        none
+// @downloadURL	 https://gitlab.emakina.net/jev/tampermonkey-scripts/-/raw/master/workfront/highlight-currentday.js
 // @updateURL	 https://gitlab.emakina.net/jev/tampermonkey-scripts/-/raw/master/workfront/highlight-currentday.js
+// @supportURL	 https://bugtracking.emakina.net/projects/ENWORKFNAV/summary
 // ==/UserScript==
 
 (function() {
