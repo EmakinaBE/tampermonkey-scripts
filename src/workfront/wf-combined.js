c19cd0c6ddc2e6b0bb1d654813ff7ee3129fa39e
Wouter Versyck <wouter.versyck@designisdead.com>
2021-02-04 11:08:16 +0100

    Added script that combines all scripts so it can be installed in 1 go

---
+++ src/workfront/wf-combined.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Combines the individual wf scripts into one
+// @namespace    https://www.emakina.com/
+// @version      1.1
+// @description  Combines the individual wf scripts into one.
+// @author       Wouter Versyck
+// @match        https://emakina.my.workfront.com/timesheet/*
+// @icon         https://emakina.my.workfront.com/static/img/favicon.ico
+// @supportURL   https://bugtracking.emakina.net/projects/ENWORKFNAV/summary
+// @homepage     https://gitlab.emakina.net/jev/tampermonkey-scripts
+// @downloadURL  https://gitlab.emakina.net/jev/tampermonkey-scripts/-/raw/master/src/workfront/wf-combined.js
+// @updateURL    https://gitlab.emakina.net/jev/tampermonkey-scripts/-/raw/master/src/workfront/wf-combined.js
+// @require      https://gitlab.emakina.net/jev/tampermonkey-scripts/-/raw/master/src/workfront/include-scripts/convert-save-into-event.js
+// @require      https://gitlab.emakina.net/jev/tampermonkey-scripts/-/raw/master/src/workfront/highlight-currentday.js
+// @require      https://gitlab.emakina.net/jev/tampermonkey-scripts/-/raw/master/src/workfront/warning-current-timesheet.js
+// @require      https://gitlab.emakina.net/jev/tampermonkey-scripts/-/raw/master/src/workfront/indicate-missing-comment.js
+// @grant        none
+// ==/UserScript==
