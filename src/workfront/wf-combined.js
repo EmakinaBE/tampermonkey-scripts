2b2c0c64cd62b16df1e64d3ffe349232b1170f54
Jeffrey Vandenbossche <jev@emakina.com>
2021-02-10 09:10:30 +0100

    Update combined script

--- src/workfront/wf-combined.js
+++ src/workfront/wf-combined.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WF combined
 // @namespace    https://www.emakina.com/
-// @version      1.5
+// @version      1.6
 // @description  Combines the individual wf scripts into one.
 // @author       Wouter Versyck
 // @match        https://emakina.my.workfront.com/timesheet/*
