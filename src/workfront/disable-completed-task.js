4d8b227376cdd6b3b764c5f327a89b8979a96a4f
Sarah Roupec <sarah.roupec@emakina.at>
2021-06-23 16:39:51 +0200

    version number++

--- src/workfront/disable-completed-task.js
+++ src/workfront/disable-completed-task.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Disable completed tasks
 // @namespace    https://www.emakina.com/
-// @version      1.5
+// @version      1.6
 // @description  Will poll the success notification after save and thrown an event. Will throw event when a new line is added
 // @author       Wouter Versyck
 // @homepage	 https://github.com/EmakinaBE/tampermonkey-scripts
