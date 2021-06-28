65301becfa532f8c86fa56e25033c1d2b0c4b525
Sarah Roupec <sarah.roupec@emakina.at>
2021-06-28 16:37:35 +0200

    Update access-document-object.js

--- src/workfront/include-scripts/access-document-object.js
+++ src/workfront/include-scripts/access-document-object.js
@@ -71,7 +71,7 @@
     window.checkIfElementExists = async (id) => {
         const elementId = id;
         const oldElement = await getElementsFromDocument(`#${elementId}`);
-        return oldElement? true : false;
+        if(oldElement) return true;
     } 
 
 })(window, document);
