564cedaf7e5c99788f6e9238b36161d38500d036
Sarah Roupec <sarah.roupec@emakina.at>
2021-06-28 17:39:14 +0200

    Update options.js

--- src/workfront/options.js
+++ src/workfront/options.js
@@ -68,7 +68,7 @@
         li.appendChild(button);
         li.classList.add('navbar-item');
         //TO-DO:
-        const navbarItemGroup = getElementsFromDocument('ul.adobe-navbar', document);
+        const navbarItemGroup = await getElementsFromDocument('ul.adobe-navbar', document);
         if(!navbarItemGroup) return;
         //const navbarItemGroup = await getElementsFromDocument('.navbar-item-group.right');
         navbarItemGroup[0].appendChild(li);
