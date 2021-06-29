bf43c1e437f9ac44d24310e335cea82a3f27a362
Sarah Roupec <sarah.roupec@emakina.at>
2021-06-29 15:00:24 +0200

    version number ++

--- src/workfront/wf-combined.js
+++ src/workfront/wf-combined.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WF combined New UI
 // @namespace    https://www.emakina.com/
-// @version      1.45
+// @version      1.46
 // @description  Combines the individual wf scripts into one.
 // @author       Wouter Versyck
 // @match        https://emakina.my.workfront.com/timesheet/*
