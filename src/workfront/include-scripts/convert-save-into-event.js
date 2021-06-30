5b5cca3a249991b94d270aa2957a40ab5f1d1711
Sarah Roupec <sarah.roupec@emakina.at>
2021-06-30 11:49:26 +0200

    Update convert-save-into-event.js

--- src/workfront/include-scripts/convert-save-into-event.js
+++ src/workfront/include-scripts/convert-save-into-event.js
@@ -34,7 +34,7 @@
 
     async function init() {
         executeCallback();
-        setTimeout(setupListeners, 100);
+        setTimeout(setupListeners, 500);
     }
 
     async function pollNetworkRequestSuccess() {
