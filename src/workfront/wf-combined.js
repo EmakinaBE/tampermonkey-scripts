750f779b01516d7f7141f79b59ae2581e4665feb
Sarah Roupec <sarah.roupec@emakina.at>
2021-07-26 11:19:45 +0200

    added matches to wf-combined

--- src/workfront/wf-combined.js
+++ src/workfront/wf-combined.js
@@ -7,6 +7,9 @@
 // @match        https://emakina.my.workfront.com/timesheet/*
 // @match        https://emakina.preview.workfront.com/timesheet/*
 // @match        https://emakina.sb01.workfront.com/timesheet/*
+// @match        https://emakina.my.workfront.com/timesheets/*
+// @match        https://emakina.preview.workfront.com/timesheets/*
+// @match        https://emakina.sb01.workfront.com/timesheets/*
 // @icon         https://emakina.my.workfront.com/static/img/favicon.ico
 // @supportURL   https://bugtracking.emakina.net/projects/ENWORKFNAV/summary
 // @homepage     https://github.com/EmakinaBE/tampermonkey-scripts
