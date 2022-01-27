9db00db6edd691aa3edd92f0bd750cd23079829d
stillday <jan-dennisd@outlook.at>
2022-01-27 14:58:13 +0100

    now is loading css

--- src/workfront/load-css.js
+++ src/workfront/load-css.js
@@ -12,10 +12,15 @@
 // @homepage     https://github.com/EmakinaBE/tampermonkey-scripts
 // @downloadURL  https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/workfront/load-css.js
 // @updateURL    https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/workfront/load-css.js
-// @grant        none
-// ==/UserScript==
+// @resource     EMAKINA_CSS https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/css/style.css
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+/// ==/UserScript==
 
 (function() {
-    console.log('hello CSS');
-    alert('gib mir css du sack')
+    'use strict';
+    console.log('Hello CSS');
+    const my_css = GM_getResourceText("EMAKINA_CSS");
+    GM_addStyle(my_css);
+    alert('Gib mir css du sack');
 })();
