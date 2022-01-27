f9e3f65c57638009a41b9d03faedfc056fb6160a
stillday <jan-dennisd@outlook.at>
2022-01-27 16:15:11 +0100

    check other type of comment for loading css

--- src/workfront/load-css.js
+++ src/workfront/load-css.js
@@ -12,8 +12,9 @@
 // @homepage     https://github.com/EmakinaBE/tampermonkey-scripts
 // @downloadURL  https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/workfront/load-css.js
 // @updateURL    https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/workfront/load-css.js
-// @grant        none
-
+// @resource     EMAKINA_CSS https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/css/style.css
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
 /// ==/UserScript==
 
 (function() {
