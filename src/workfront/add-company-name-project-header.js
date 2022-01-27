a2c686a5cc5be2f79d2b166205052519e6f84806
stillday <jan-dennisd@outlook.at>
2022-01-27 12:29:32 +0100

    change file url

--- src/workfront/add-company-name-project-header.js
+++ src/workfront/add-company-name-project-header.js
@@ -11,8 +11,8 @@
 // @icon         https://emakina.my.workfront.com/static/img/favicon.ico
 // @supportURL   https://bugtracking.emakina.net/projects/ENWORKFNAV/summary
 // @homepage     https://github.com/EmakinaBE/tampermonkey-scripts
-// @downloadURL  https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/New-UI/src/workfront/add-company-name-project-header.js
-// @updateURL    https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/New-UI/src/workfront/add-company-name-project-header.js
+// @downloadURL  https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/workfront/add-company-name-project-header.js
+// @updateURL    https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/workfront/add-company-name-project-header.js
 // @grant        none
 // ==/UserScript==
 
