3db165b26e9794cf959dfc5d910d02ce3cbce51e
stillday <jan-dennisd@outlook.at>
2022-01-27 15:43:49 +0100

    change file url

--- src/workfront/load-css.js
+++ src/workfront/load-css.js
@@ -12,14 +12,14 @@
 // @homepage     https://github.com/EmakinaBE/tampermonkey-scripts
 // @downloadURL  https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/workfront/load-css.js
 // @updateURL    https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/workfront/load-css.js
+// @resource     EMAKINA_CSS https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/css/style.css
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
-// @resource     EMAKINA_CSS https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/feature/ENWORKFNAV-2986-tm-mark-save-and-close-b/src/css/style.css
 /// ==/UserScript==
 
 (function() {
-    console.log('Hello CSS');
+    'use strict';
+    console.log('Hello');
     const my_css = GM_getResourceText("EMAKINA_CSS");
     GM_addStyle(my_css);
-    alert('Gib mir css du sack');
 })();
