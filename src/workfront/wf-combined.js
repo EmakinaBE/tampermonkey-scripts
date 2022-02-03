dc8df121e6da5b2209f6c6dc4386dabf7aa49917
stillday <jan-dennisd@outlook.at>
2022-02-03 16:35:36 +0100

    main file. include at package

--- src/workfront/wf-combined.js
+++ src/workfront/wf-combined.js
@@ -15,6 +15,8 @@
 // @homepage     https://github.com/EmakinaBE/tampermonkey-scripts
 // @downloadURL  https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/master/src/workfront/wf-combined.js
 // @updateURL    https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/master/src/workfront/wf-combined.js
+// @require      https://raw.githubusercontent.com/EmakinaBE/tampermonkey-scripts/test3/src/workfront/options.js
+
 
 // ==/UserScript==
 
