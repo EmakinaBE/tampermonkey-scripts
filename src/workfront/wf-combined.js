6ce4a4b33a95bdc89301f0c4537a738e4725a167
stillday <jan-dennisd@outlook.at>
2022-02-11 14:53:13 +0100

    Versions Number rollback

--- src/workfront/wf-combined.js
+++ src/workfront/wf-combined.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         WF combined New
+// @name         WF combined
 // @namespace    https://www.emakina.com/
-// @version      2.2.0.4
+// @version      1.45
 // @description  Combines the individual wf scripts into one.
 // @author       Wouter Versyck
 // @match        https://emakina.my.workfront.com/timesheet/*
