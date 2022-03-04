6e83d4be50ca01cc9879871c98318744c957da90
JD <jan-dennisd@outlook.at>
2022-03-04 15:13:41 +0100

    Update wf-combined.js

--- src/workfront/wf-combined.js
+++ src/workfront/wf-combined.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         WF combined api and design
+// @name         WF combined
 // @namespace    https://www.emakina.com/
 // @version      2.2.1.0
 // @description  Combines the individual wf scripts into one.
