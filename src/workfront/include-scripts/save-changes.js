e67ee89f51cf0656cc845665c3243d92e9409efe
Jan Drenkhahn <jan.drenkhahn@emakina.at>
2022-07-01 12:12:20 +0200

    change check for version and date

--- src/workfront/include-scripts/save-changes.js
+++ src/workfront/include-scripts/save-changes.js
@@ -30,8 +30,7 @@
 
     var currentVersionCheck = GM_info.script.version;
 
-    if(isInThePast(new Date('2022-07-05')) && currentVersionCheck <= "2.2.1.13") {
-        console.log('mep');
+    if(isInThePast(new Date('2022-07-11')) && currentVersionCheck <= "2.2.2.0") {
         setTimeout(async() => {
             var newDiv = document.createElement("div");
             newDiv.innerHTML = "Your Tampermonkey script is out-dated, please update it. (Details <a href='https://share.emakina.net/display/ENWNI/Tampermonkey+Script#TampermonkeyScript-ManualUpdating' style='text-decoration:underline;padding-left: 5px' target='_blank'>see our documentation</a>)";
