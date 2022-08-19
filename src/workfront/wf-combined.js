3e3c5106fb7f56772c383519d72ef8f71eeb2ef0
Jan Drenkhahn <jan.drenkhahn@emakina.at>
2022-08-19 11:14:09 +0200

    version update

--- src/workfront/wf-combined.js
+++ src/workfront/wf-combined.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WF combined test
 // @namespace    https://www.emakina.com/
-// @version      3.0.1.20
+// @version      3.0.1.21
 // @description  Combines the individual wf scripts into one.
 // @author       Wouter Versyck, Jan-Dennis Drenkhahn
 // @match        https://emakina.my.workfront.com/*
